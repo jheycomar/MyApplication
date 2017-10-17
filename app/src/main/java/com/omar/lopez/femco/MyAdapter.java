d18b13cf9af9947e817a73039090d8c4058e62d1
Omar <jheycovich@gmail.com>
2017-10-17 17:34:47 -0400

    add LisView customer and onclick

---
+++ app/src/main/java/com/omar/lopez/femco/MyAdapter.java
@@ -0,0 +1,69 @@
+package com.omar.lopez.femco;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import java.util.List;
+
+public class MyAdapter extends BaseAdapter {
+
+    private Context context;
+    private  int layout;
+    private List<String> names;
+
+    public  MyAdapter(Context context, int layout,List<String>names){
+        this.context=context;
+        this.layout=layout;
+        this.names=names;
+    }
+
+    @Override
+    public int getCount() {
+        return this.names.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return this.names.get(position);
+    }
+
+    @Override
+    public long getItemId(int id) {
+        return id;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup viewGroup) {
+        //View Holder pattern (patron View Holder)
+       ViewHolder holder;
+        if (convertView==null){
+
+            //inflamos la vista que nos a llegado con nuestro layout personalisado
+            LayoutInflater layoutInflater=LayoutInflater.from(this.context);
+            convertView=layoutInflater.inflate(R.layout.list_item,null);
+            holder=new ViewHolder();
+            //referenciamos al elemento amodificar y lo remplasamos
+            holder.nameTextView= (TextView) convertView.findViewById(R.id.nombre);
+            convertView.setTag(holder);
+        }
+        else{
+            //recuperamos el viewholder
+            holder=(ViewHolder)convertView.getTag();
+        }
+       //nos traemos el valor dependiendo de la posicion
+        String currentName=names.get(position);
+        //referenciamos al elemento amodificar y lo remplasamos
+        holder.nameTextView.setText(currentName);
+        //devolvemos la vista inflada y modificada con nuestros datos
+        return convertView;
+
+    }
+    static class  ViewHolder{
+        private TextView nameTextView;
+    }
+
+}
--- app/src/main/java/com/omar/lopez/femco/SecondActivity.java
+++ app/src/main/java/com/omar/lopez/femco/SecondActivity.java
@@ -1,25 +1,52 @@
 package com.omar.lopez.femco;
 
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class SecondActivity extends AppCompatActivity {
-private TextView textView;
+    private TextView textView;
+    private ListView listView;
+    List<String> nombres;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
 
         // avilita el la flecha de regreso
-       // Toolbar toolbar = (Toolbar) findViewById(R.id.appbar);
-       // setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayShowTitleEnabled(true);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
+        //enlazamos los controles
         textView=(TextView) findViewById(R.id.txtsecond);
+        listView=(ListView)findViewById(R.id.lstView);
+
+        //datos a mostrar
+        nombres=new ArrayList<String>();
+        nombres.add("fercho");
+        nombres.add("alien");
+        nombres.add("florencio");
+        nombres.add("Ariel");
+        nombres.add("Melissa");
+        nombres.add("Wendy");
+
+         //la forma visual enque se va a mostrar
+      //  ArrayAdapter<String> adaptador=new ArrayAdapter<String>(this ,android.R.layout.simple_list_item_1,nombres);
+        //enlazamos nuestro adaptador con nuestro lisView
+       // listView.setAdapter(adaptador);
+
         //tomar los datos del intent
         Bundle bundle=getIntent().getExtras();
         if (  bundle!=null && bundle.getString("llave")!=null){
@@ -29,5 +56,21 @@ private TextView textView;
         }else {
             Toast.makeText(SecondActivity.this,"this message is null",Toast.LENGTH_LONG).show();
         }
+
+        //click en el iten del listView
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
+                Snackbar.make(view,""+nombres.get(position),Snackbar.LENGTH_LONG).setAction("Action",null).show();
+
+            }
+        });
+
+        //instancia del adaptador
+        MyAdapter myAdapter=new MyAdapter(this,R.layout.list_item,nombres);
+        listView.setAdapter(myAdapter);
+
     }
+
 }
+
